16439e0a7149490a8986318a5d500052ff5cad7a
Igor21v <igor21v@mail.ru>
2022-01-16 19:39:18 +0800

    Добавлено управление состояниями Redux

--- App.js
+++ App.js
@@ -2,28 +2,32 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navibar from './Components/Navibar';
 import Footer from './Components/Footer';
+import { Provider } from 'react-redux';
+import { store } from "./store"
 
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
-import {Themes} from './Pages/Themes';
-import {About} from './Pages/About';
-import {Home} from './Pages/Home';
+import { Themes } from './Pages/Themes';
+import { About } from './Pages/About';
+import { Home } from './Pages/Home';
 
 function App() {
   return (
     <>
-      <Router>
-        <Navibar />
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/themes" element={<Themes/>} />
-          <Route path="/about" element={<About/>} />
-        </Routes>
-      </Router>
-      <Footer/>
+      <Provider store={store}>
+        <Router>
+          <Navibar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/themes" element={<Themes />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </Router>
+        <Footer />
+      </Provider>
     </>
   );
 }
--- Pages/Home.js
+++ Pages/Home.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { Profile } from '../Components';
+import { LoginForm } from '../Components';
 
 export const Home = () => (
-            <h1>Личный кабинет учителя/ученика</h1>
-    )
- 
+        <>      
+                <h1>Личный кабинет учителя/ученика</h1>
+                <Profile />
+                <LoginForm/>
+        </>
+)
