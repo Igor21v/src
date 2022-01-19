d0afd5db3cff8296d4d06ead5ec8012558e9bb48
Igor21v <igor21v@mail.ru>
2022-01-19 22:10:02 +0800

    1

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from "redux";
 import { redusers } from "./redusers";
+import { composeWithDevTools } from 'redux-devtools-extension'
 
-export const store = createStore(redusers);
+export const store = createStore(redusers, composeWithDevTools());
